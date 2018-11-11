ec900d1a94edcf2d455f91d53dd616a787db3e28
Francisco Machado <machado.fran@gmail.com>
2018-11-11 15:29:37 -0300

    fix footer en resoluciones muy chicas

--- application/views/bloques/footer.php
+++ application/views/bloques/footer.php
@@ -1,6 +1,6 @@
 <footer class="row">
     <div id="publicar_evento_footer" class="col-sm-12">
-        <div class="hold hidden-sm hidden-xs">
+        <div class="hold hidden-sm hidden-xs hidden-xxs">
             <a href="<?php echo base_url() ?>app/nuevo" class="organizadores-btn"><img src="<?php echo base_url()?>assets/global/imgs/botones/organizadores.gif" alt="Publica tu evento gratis"></a>
         </div><div class="hold">
         <a href="<?php echo base_url() ?>app/nuevo" class="icono-footer organizador-button hidden-md hidden-lg hidden-xl">Organizadores</a> <a href="twitter" class="fa fa-twitter icono-footer social-button"></a> <a href="google" class="fa fa-google-plus icono-footer social-button"></a> <a href="email" class="fa fa-envelope icono-footer"></a>
